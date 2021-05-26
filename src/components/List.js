7de70f93e03dcbedf25cbb67af20c452161662ac
Pascal Pater <pascal.pater@gmail.com>
2021-05-26 14:47:46 +0200

    Create and render List component

---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class List extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <ul>
+        <li>Melk</li>
+        <li>Brood</li>
+        <li>Honing</li>
+      </ul>
+    )}
+    
+}
+
+export default List;
