5056faaba365df700ce2fc8816adaaa3c4324f18
Pascal Pater <pascal.pater@gmail.com>
2021-05-31 15:16:58 +0200

    move state to Container component

--- src/components/List.js
+++ src/components/List.js
@@ -2,16 +2,7 @@ import React, {Component} from "react";
 import ListItem from './ListItem'
 
 class List extends Component {
-  constructor() {
-    super()
-    this.state = {
-      groceryItems: [
-          { id: 1, title: "Melk" },
-          { id: 2, title: "Brood" },
-          { id: 3, title: "Rijst"}
-      ]
-    }
-  }
+
 
   render() {
     const listItem = this.state.groceryItems.map((groceryItem) => (
