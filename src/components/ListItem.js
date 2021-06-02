2ddb018905347d4e84a09f41f69188797108d434
Pascal Pater <pascal.pater@gmail.com>
2021-06-02 15:06:30 +0200

    add ternary operator on onClick to prevent error message when clicked on item from shoppingCart

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,7 +5,8 @@ function ListItem(props) {
     <li
       key={props.item.id}
       value={props.item.title}
-      onClick={() => props.click(props.item.title)}
+      // ternary operator needed to prevent error message when clicked on item from shoppingCart
+      onClick={() => props.click ? props.click(props.item.title) : null}
     >
       {props.item.title}
     </li>
