a58bf8383df034ce15d9894a579dfc05a774ce09
Pascal Pater <pascal.pater@gmail.com>
2021-06-03 18:45:53 +0200

    add header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Pascal Pater - React opdracht 1 Boodschappenlijst & Winkelmand voor Winc Academy Front-end Development opleiding</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="page-header">
+      <h1>Boodschappenlijst</h1>
+    </header>
+  )
+}
+
+export default Header
