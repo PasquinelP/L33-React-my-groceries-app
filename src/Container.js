fb69abd24a188ba000fd44ba1e4376e78da30565
Pascal Pater <pascal.pater@gmail.com>
2021-06-11 21:10:43 +0200

    conver handleClickGroceryItem and addAmountToItem functions to work with useState hook

--- src/Container.js
+++ src/Container.js
@@ -5,21 +5,6 @@ import ShoppingCart from "./components/ShoppingCart";
 import Footer from './components/Footer';
 
 const Container = () => {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     groceryItems: [
-  //       { id: 1, title: "Melk" },
-  //       { id: 2, title: "Brood" },
-  //       { id: 3, title: "Rijst" },
-  //     ],
-  //     shoppingListItems: [],
-  //   };
-  //   this.handleClickGroceryItem = this.handleClickGroceryItem.bind(this);
-  //   this.emptyCart = this.emptyCart.bind(this);
-  //   this.handleClickAddGrocery = this.handleClickAddGrocery.bind(this);
-  //   this.addAmountToItem = this.addAmountToItem.bind(this);
-  // }
 
   const [groceryItems, setGroceryItems] = useState([
         { id: 1, title: "Melk" },
@@ -27,50 +12,41 @@ const Container = () => {
         { id: 3, title: "Rijst" },
       ]);
 
-  const [shoppingListItems, setShoppingListItems] = useState([
-    { id: 1, title: "Test item 1" },
-  ]);
+  const [shoppingListItems, setShoppingListItems] = useState([]);
 
   // check if item is already in shoppingList
   // if so, on clicking same grocery item again will not add it again to he list
   // but will increment the count of amount of the item in shopping list
-  // addAmountToItem(title) {
-  //   const currentShoppingList = [...this.state.shoppingListItems];
-  //   const newShoppingList = currentShoppingList.map((item) => {
-  //     if (item.title === title) {
-  //       item.amount++;
-  //     }
-  //     return item;
-  //   });
-  //   this.setState({ shoppingListItems: newShoppingList });
-  // }
+  const addAmountToItem = (title) => {
+    const newShoppingListItem = shoppingListItems.map((item) => {
+      if (item.title === title) {
+        item.amount++;
+      }
+      return item;
+    });
+    setShoppingListItems(newShoppingListItem);
+  }
 
   // check if item in grocery list is clicked
   // check if clicked item allready exists in shopping list
   // if not, add clicked item to shopping list
   // if it is, call addAmountToItem to only increment the amount of the item in shopping list
-  // handleClickGroceryItem(title) {
-  //   const shoppingListItems = [...this.state.shoppingListItems];
-  //   const shoppingListItem = shoppingListItems.filter(
-  //     (item) => item.title === title
-  //   );
+  const handleClickGroceryItem = (title) => {
+    const shoppingListItem = shoppingListItems.filter(
+      (item) => item.title === title
+    );
 
-  //   if (shoppingListItem.length === 0) {
-  //     this.setState((prevState) => {
-  //       const newShoppingList = prevState.shoppingListItems;
-  //       newShoppingList.push({
-  //         id: newShoppingList.length + 1,
-  //         title: title,
-  //         amount: 1,
-  //       });
-  //       return {
-  //         shoppingListItems: newShoppingList,
-  //       };
-  //     });
-  //   } else {
-  //     this.addAmountToItem(title);
-  //   }
-  // }
+    if (shoppingListItem.length === 0) {
+      const newShoppingItem = {
+        id: shoppingListItems.length + 1,
+        title: title,
+        amount: 1,
+      };
+      setShoppingListItems(prevShoppingListItems => [...prevShoppingListItems, newShoppingItem])
+    } else {
+      addAmountToItem(title);
+    }
+  }
 
   // setState of shoppingListItems to empty array to clear list
   const emptyCart = () => {
@@ -97,7 +73,7 @@ const Container = () => {
       <main>
         <GroceryList
           groceryItems={groceryItems}
-          // clickItem={this.handleClickGroceryItem}
+          clickItem={handleClickGroceryItem}
           addGrocery={handleClickAddGrocery}
         />
         <ShoppingCart
