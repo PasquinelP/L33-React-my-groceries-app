ec8fef37a1fa632a6c6afcdf8d7db0d316b4459f
Pascal Pater <pascal.pater@gmail.com>
2021-06-11 18:55:57 +0200

    convert input field from class component to functional component to use useState hook

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,39 +1,31 @@
-import React, { Component } from 'react'
+import React, { useState }  from 'react'
 
-class InputField extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
+const InputField = (props) => {
+
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
   }
 
-  handleChange(event) {
-    this.setState({ title: event.target.value });
-  }
-
-
-  render() {
-    return (
-      // make sure to setState to empty title to clear input field after submit
-      <form
-        onSubmit={(e) => {
-          this.props.onSubmit(e, this.state.title);
-          this.setState({ title: "" });
-        }}
-      >
-        <input
-          type="text"
-          name="title"
-          value={this.state.title}
-          onChange={this.handleChange}
-          placeholder="Vul je boodschappen in"
-        />
-        <button type="submit">Voeg toe</button>
-      </form>
-    );
-  }
+  return (
+    // make sure to setTitle to empty title to clear input field after submit
+    <form
+      onSubmit={(e) => {
+        props.onSubmit(e, title);
+        setTitle("");
+      }}
+    >
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={handleChange}
+        placeholder="Vul je boodschappen in"
+      />
+      <button type="submit">Voeg toe</button>
+    </form>
+  );
 }
 
 export default InputField
